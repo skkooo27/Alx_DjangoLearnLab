763cf2072f68271aed350ee7166af6cd99719e7d
skkooo27 <sharlinekerubo27@gmail.com>
2025-09-07 09:44:38 +0300

    "Added import from django.views.generic.detail and removed unused import from django.views.generic"

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,7 +3,8 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import  permission_required
 from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
-from django.views.generic import DetailView
+
+from django.views.generic.detail import DetailView
 from django.urls import reverse_lazy
 from .models import Library
 from .models import Book, Library, UserProfile
