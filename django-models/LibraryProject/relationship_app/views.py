36fa1affcd3d52721f2d96f2af1a0406737f9a4a
skkooo27 <sharlinekerubo27@gmail.com>
2025-09-07 09:23:02 +0300

    `Replaced custom authentication views with built-in Django views`

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.views import LoginView, LogoutView
+
 from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
 from django.views.generic import DetailView
 from django.urls import reverse_lazy
@@ -17,13 +17,6 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
-
-# Authentication views
-class CustomLoginView(LoginView):
-    template_name = 'relationship_app/login.html'
-
-class CustomLogoutView(LogoutView):
-    template_name = 'relationship_app/logout.html'
 
 def register(request):
     if request.method == 'POST':
