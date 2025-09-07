1ca3eeb35109df58d9fcfa737d66f5a44df9bd92
skkooo27 <sharlinekerubo27@gmail.com>
2025-09-07 09:48:29 +0300

    "Removed user_passes_test decorator from import statement in views.py"

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.decorators import  permission_required
 from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
+from django.contrib.auth.decorators import permission_required
 
 from django.views.generic.detail import DetailView
 from django.urls import reverse_lazy
