a6747592fd32bf180a514a1c532df5c9b7bacc22
skkooo27 <sharlinekerubo27@gmail.com>
2025-09-07 09:28:12 +0300

    `Removed TODO.md file and updated views.py to include necessary imports and decorators.`

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib.auth.decorators import  permission_required
 from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
 from django.views.generic import DetailView
 from django.urls import reverse_lazy
+from .models import Library
 from .models import Book, Library, UserProfile
 
 # Function-based view to list all books
