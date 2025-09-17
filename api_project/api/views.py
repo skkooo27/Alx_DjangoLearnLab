0e5aed4db019e6b1a498d48e64a54b6d7bbed6f8
skkooo27 <sharlinekerubo27@gmail.com>
2025-09-17 14:21:40 +0300

    Implemented tasks 2 and 3 of TODO.md, added token authentication and permissions to API

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
 
@@ -9,3 +10,4 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
